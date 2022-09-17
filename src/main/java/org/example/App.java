adf20d16523da42f298f45c6e69e561437396a6a
Soundmaker <soundmakersoz@gmail.com>
2022-09-17 17:59:21 +0300

    Save in database with Hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,9 +14,13 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+            Person person1 = new Person("ivan",33);
+            Person person2 = new Person("Boris",34);
+            Person person3 = new Person("Ali",32);
+
+            session.save(person1);
+            session.save(person2);
+            session.save(person3);
 
             session.getTransaction().commit();
         } finally {

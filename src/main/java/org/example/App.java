2ce770490ec13daf930003470fa01e30b576b67f
Soundmaker <soundmakersoz@gmail.com>
2022-09-22 19:01:09 +0300

    Many-to-Many in Hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,25 +1,35 @@
 package org.example;
 
-import org.example.model.Passport;
-import org.example.model.Person;
+import org.example.model.Actor;
+import org.example.model.Movie;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class App {
-    public static void main( String[] args ) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
-        SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-        try {
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class)
+                .addAnnotatedClass(Movie.class);
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            session.getTransaction().commit();
+            Actor actor = session.get(Actor.class,2);
+            System.out.println(actor.getMovies());
 
-        } finally {
-            sessionFactory.close();
+            Movie movieToRemove = actor.getMovies().get(0);
+
+            actor.getMovies().remove(0);
+            movieToRemove.getActors().remove(actor);
+
+
+            session.getTransaction().commit();
         }
 
     }
